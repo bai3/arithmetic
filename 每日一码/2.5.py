3e6a4e794e004a2a0f946dbb013064f154397a50
bai3 <2442694004@qq.com>
2018-01-18 21:57:49 +0800

    :fire: 2018/1/18

---
+++ 每日一码/2.5.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, node):
+        # write code here
+        self.stack1.append(node)
+
+    def pop(self):
+        # return xx
+        if self.stack2 == []:
+            while self.stack1:
+                a = self.stack1.pop()
+                self.stack2.append(a)
+        if self.stack2:
+            return self.stack2.pop()
